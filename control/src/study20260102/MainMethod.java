f62b70276fba061b95e4d778a1cfc14d2a15ced3
DW-203 <DW-203@DESKTOP-LQS2VIH>
2026-01-02 17:31:29 +0900

    1월2일날 한내용

---
+++ control/src/study20260102/MainMethod.java
@@ -0,0 +1,47 @@
+package study20260102;
+
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		 
+		if(10>5 ) {
+			System.out.println(" 5보다 크다");	
+		}else {
+			System.out.println(" 5보다 작다");
+		}
+		
+		
+		//바이킹 탑승 가능 : 145cm 이상
+		
+		int tall= 160;
+		
+		if(tall >= 145) {
+			System.out.println("바이킹 탑승 가능");
+		}else {
+			System.out.println("바이킹 탑승 불가");
+			
+		}
+	
+		int age =33;
+		if((age/10) ==3 )
+//		if( age>=30 && age <40)
+			System.out.println("40대 이다");
+		else
+		    System.out.println("30대 이다");
+	
+	
+	
+		//if 문
+		// 주차장에 요금이 기본요금은 1000원입니다.
+		//기본시간은 30분입니다.
+		//10분 당 100원씩 추가  예)39분은 1000원 40분은 1100원
+		//단 4시간 이상 주차시 기본요금은 2500원이다.
+		// 예) 3시간 50분(230분) - 3000원
+		//     4시간 12분(252분) ->2600원
+		
+	
+	
+	}
+
+}
