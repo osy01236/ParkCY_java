620ac5e2e02aa8aac8d9d5f6d09b02274ce26622
osy01 <osy01@DESKTOP-6VN833I>
2026-01-05 23:23:44 +0900

    0105 11시 23분 과제 끝

--- control/src/test20260105/test01.java
+++ control/src/test20260105/test01.java
@@ -2,6 +2,8 @@ package test20260105;
 
 import java.util.Scanner;
 
+
+
 public class test01 {
 
 	public static void main(String[] args) {
@@ -11,29 +13,40 @@ public class test01 {
 //		게임을 진행 하면서 1판 끝날때 마다 
 //		몇승 몇패 몇무 출력하기
 
-		int count =0;
-		int count2=0;
-		System.out.print("1=가위, 2=바위, 3=보");
+		System.out.print("가위 바위 보 게임을 진행합니다.\n1=가위, 2=바위, 3=보 ");
 		
-		
-		
-		
-		
-		
-		for(int i = 1 ; i>=15; i++) {
-			int com = (int)Math.floor(Math.random()*3)+1;
-			int user = (int)Math.floor(Math.random()*3)+1;
-			
+//		Scanner sc= new Scanner(System.in);
+	
+		int count = 0;
+		int count2 = 0;
+		int count3 = 0;
+
+		for (int i = 1; i <= 15; i++) {
+			int com = (int) Math.floor(Math.random() * 3) + 1; // 컴퓨터 가위 바위 보
+			int user = (int) Math.floor(Math.random() * 3) + 1; // 사용자 가위 바위 보
+//			int user = sc.nextInt();
+
+			System.out.println("\n==========");
+			System.out.println("컴퓨터 : " + com + " :"); // 컴퓨터 값
+			System.out.println("사용자 : " + user + " :"); // 사용자 값
+			System.out.println("==========");
+
 			int num = user - com;
-			if(num == 0 ) {
+
+			if (num == 0) { // 무승부 조건 num 값이 0일 경우
 				count++;
-				System.out.println("무승부");
-			}else if(num == -2 || num == 1);
+				System.out.println("결과 : 무승부");
+			} else if (num == -2 || num == 1) { // 승리 조건 num이 -2 또는 1일 경우
 				count2++;
-			    System.out.println("나의 승리");
-			
+				System.out.println("결과 : 승리");
+			} else {
+				count3++; // 그 외 조건 패배 ?
+				System.out.println("결과 : 패배");
+			}
+
+			System.out.println("\n무승부 :" + count + "무" + " 승리 :" + count2 + "승" + " 패배 :" + count3 + "패");
+
 		}
-		System.out.println("무승부 :" + count);
-		System.out.println("나의 승리 :" + count2);
-	}		
+	}
+
 }
